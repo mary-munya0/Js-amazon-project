97290f49777b535da24f615f6f4740d43788d29f
mary-munya0 <maryjunemunyao@gmail.com>
2024-10-18 19:49:59 +0300

    Generate HTML for the order summary

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,8 +1,9 @@
 import{cart} from '../data/cart.js';
 import { products } from '../data/products.js';
 
-cart.forEach((cartItem)=>{
+let cartSummaryHTML ='';
 
+cart.forEach((cartItem)=>{
     const productId = cartItem.productId;
     let matchingProduct;
 
@@ -12,8 +13,8 @@ cart.forEach((cartItem)=>{
         }
     });
 
-    console.log(matchingProduct);
-    `
+   
+    cartSummaryHTML +=`
       <div class="cart-item-container">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
@@ -21,18 +22,18 @@ cart.forEach((cartItem)=>{
 
             <div class="cart-item-details-grid">
               <img class="product-image"
-                src="">
+                src="${matchingProduct.image}">
 
               <div class="cart-item-details">
                 <div class="product-name">
-                  Black and Gray Athletic Cotton Socks - 6 Pairs
+                  ${matchingProduct.name}
                 </div>
                 <div class="product-price">
-                  $10.90
+                  ${(matchingProduct.priceCents / 100).toFixed(2)}
                 </div>
                 <div class="product-quantity">
                   <span>
-                    Quantity: <span class="quantity-label">2</span>
+                    Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                   </span>
                   <span class="update-quantity-link link-primary">
                     Update
@@ -89,5 +90,8 @@ cart.forEach((cartItem)=>{
               </div>
             </div>
           </div>
-    `
+    `;
 });
+
+document.querySelector('.js-order-summary')
+    .innerHTML = cartSummaryHTML;
