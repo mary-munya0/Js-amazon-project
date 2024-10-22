54626f30f8f5ffacd9c2c1b3960cceef626a0b74
mary-munya0 <maryjunemunyao@gmail.com>
2024-10-22 19:17:23 +0300

    Make delivery options interactive

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,4 +1,4 @@
-import{cart,removeFromCart} from '../data/cart.js';
+import{cart,removeFromCart,updateDeliveryOption} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -85,6 +85,52 @@ const dateString =deliveryDate.format('dddd, MMMM D');
     `;
 });
 
+function deliveryOptionsHTML(matchingProduct,cartItem){
+  let html ='';
+  
+    deliveryOptions.forEach((deliveryOption)=>{
+      const today =dayjs();
+      const deliveryDate = today.add(
+        deliveryOption.deliveryDays,
+        'days'
+      );
+      const dateString =deliveryDate.format('dddd, MMMM D');
+  
+      const priceString = deliveryOption.priceCents 
+      === 0
+        ?'FREE'
+        :`$${formatCurrency(deliveryOption.priceCents)} -`;
+  
+  
+      const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+  
+  
+      html +=
+  
+      `
+        <div class="delivery-option js-delivery-option"
+              data-product-id="${matchingProduct.id}"
+              data-delivery-option-id="${deliveryOption.id}">
+            <input type="radio"
+              ${isChecked ? 'checked' : ''}
+              class="delivery-option-input"
+              name="delivery-option-${matchingProduct.id}">
+            <div>
+              <div class="delivery-option-date">
+                ${dateString}
+              </div>
+              <div class="delivery-option-price">
+                ${priceString}  Shipping
+              </div>
+            </div>
+          </div>
+          
+      `;
+    });
+  
+    return html;
+  }
+
 document.querySelector('.js-order-summary')
     .innerHTML = cartSummaryHTML;
 
@@ -103,48 +149,13 @@ document.querySelectorAll('.js-delete-link')
         });
     });
 
-function deliveryOptionsHTML(matchingProduct,cartItem){
-let html ='';
+document.querySelectorAll('.js-delivery-option')
+    .forEach((element)=>{
+      element.addEventListener('click',()=>{
+        const{productId,deliveryOptionId}=element.dataset;
+        updateDeliveryOption(productId,deliveryOptionId);
+      });
+    });
 
-  deliveryOptions.forEach((deliveryOption)=>{
-    const today =dayjs();
-    const deliveryDate = today.add(
-      deliveryOption.deliveryDays,
-      'days'
-    );
-    const dateString =deliveryDate.format('dddd, MMMM D');
-
-    const priceString = deliveryOption.priceCents 
-    === 0
-      ?'FREE'
-      :`$${formatCurrency(deliveryOption.priceCents)} -`;
-
-
-    const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
-
-
-    html +=
-
-    `
-      <div class="delivery-option">
-          <input type="radio"
-            ${isChecked ? 'checked' : ''}
-            class="delivery-option-input"
-            name="delivery-option-${matchingProduct.id}">
-          <div>
-            <div class="delivery-option-date">
-              ${dateString}
-            </div>
-            <div class="delivery-option-price">
-              ${priceString}  Shipping
-            </div>
-          </div>
-        </div>
-        
-    `
-  });
-
-  return html;
-}
 
 
